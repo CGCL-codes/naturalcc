7f93173c78aa9ca568ec1f1c414ae29564a0dee7
timetub <63572118+timetub@users.noreply.github.com>
2022-05-10 16:00:25 +0800

    Update model.py

--- Interpretability/code_probe_structural/structural-probes/model.py
+++ Interpretability/code_probe_structural/structural-probes/model.py
@@ -1,5 +1,4 @@
 """Classes for constructing word representations."""
-"""用于构造单词表示形式的类。"""
 import torch
 import torch.nn as nn
 
@@ -19,10 +18,8 @@ class Model(nn.Module):
 
 class DiskModel(Model):
   '''A class for providing pre-computed word representations.
-  用于提供预先计算的单词表示形式的类。
   Assumes the batch is constructed of loaded-from-disk
   embeddings.
-  假定该批处理由从磁盘加载的嵌入构成。
   '''
 
   def __init__(self, args):
