d5ac260e461b9f2dd6afd5315373c82a811370d7
3usi9 <Templ_1@outlook.com>
2022-10-14 15:28:13 +0800

    add typilus dataset

---
+++ ncc_dataset/typilus/README.md
@@ -0,0 +1,15 @@
+# Dataset: Typilus
+
+<hr>
+
+# Step 1
+Download pre-processed and raw (python_wan) dataset.
+```shell
+TBC
+```
+
+# Step 2
+Dataset preprocessing
+```shell
+python -m dataset.typilus.preprocess.preprocess
+```
---
+++ ncc_dataset/typilus/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import os
+from ncc import (
+    __NCC_DIR__,
+    __BPE_DIR__, __TREE_SITTER_LIBS_DIR__,
+)
+from ncc.data.constants import (
+    RAW, ATTRIBUTES, MODES,
+)
+
+DATASET_NAME = 'typilus'
+DATASET_DIR = os.path.join(__NCC_DIR__, DATASET_NAME)
+
+RAW_DIR = os.path.join(DATASET_DIR, RAW)
+ATTRIBUTES_DIR = os.path.join(DATASET_DIR, ATTRIBUTES)
+BPE_DIR = __BPE_DIR__
+LIBS_DIR = __TREE_SITTER_LIBS_DIR__
+
+LANGUAGES = ['c']
+
+__all__ = (
+    "DATASET_NAME",
+    "RAW_DIR", "LIBS_DIR", "ATTRIBUTES_DIR",
+    "LANGUAGES", "MODES",
+)
