919813ba9cebefe1104bc63239d468c2be524001
Weixi Tong <weixitong@hust.edu.cn>
2023-10-22 21:38:31 +0800

    [fix] move to third-party

--- third_party/metrics_evaluation/metrics/__init__.py
+++ third_party/metrics_evaluation/metrics/__init__.py
@@ -5,3 +5,4 @@ from .sacrebleu_code.sacrebleu_methods.compat import sentence_chrf
 from .sacrebleu_code.sacrebleu_methods.compat import sentence_bleu
 from rouge_score import rouge_scorer as rouge
 from nltk.translate.meteor_score import single_meteor_score as meteor
+#humaneval mbpp
