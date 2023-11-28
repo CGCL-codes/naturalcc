5e1d81a426b26526c146be33b139726ba8687d57
uniqueFranky <y_r_b@qq.com>
2023-11-28 16:26:52 +0800

    add plugin completion context

--- plugin/src/completion.ts
+++ plugin/src/completion.ts
@@ -33,10 +33,42 @@ export class NccCompletionProvider implements InlineCompletionItemProvider {
         return new Promise((resolve) => {
           this.debounceTimer = setTimeout(async () => {
             console.log('trigger')
-            const ret = await getCompletion(line)
+            const ctx = getContext(document, position)
+            const ret = await getCompletion(ctx)
             const completionRange = document.lineAt(position.line).range
-            resolve([new InlineCompletionItem(ret, completionRange)])
+            const compl = removeFirstNLines(ret, ctx.split('\n').length - 1)
+            console.log(ctx)
+            console.log(ctx.split('\n').length)
+            console.log(compl)
+            resolve([new InlineCompletionItem(compl, completionRange)])
           }, Number(getConfig().debounce));
         });
     }
+}
+
+
+function getContext(document: TextDocument, position: Position): string {
+  const cnt = Math.min(getConfig().contextLineCount, position.line + 1)
+  let ctx = ""
+  for(let i = cnt - 1; i >= 0; i--) {
+    const line = document.lineAt(position.line - i)
+    if(!line.isEmptyOrWhitespace) {
+      ctx += line.text
+      if(i > 0) {
+        ctx += '\n'
+      }
+    }
+  }
+  return ctx
+}
+
+function removeFirstNLines(str: string, n: number): string {
+  const lines = str.split('\n');
+  if(n >= lines.length) {
+      return '';
+  }
+  if(n <= 0) {
+    return str
+  }
+  return lines.slice(n).join('\n');
 }
--- plugin/src/config.ts
+++ plugin/src/config.ts
@@ -2,28 +2,31 @@ import { workspace } from 'vscode'
 import { pluginName } from './extension'
 export class NccConfig {
     public server: string
-    public debounce: Number
-    public maxLength: Number
-    public topK: Number
-    public topP: Number
-    public temperature: Number
+    public debounce: number
+    public maxLength: number
+    public topK: number
+    public topP: number
+    public temperature: number
+    public contextLineCount: number
     constructor() {
         this.server = workspace.getConfiguration(pluginName).get('server') as string
-        this.debounce = workspace.getConfiguration(pluginName).get('debounce') as Number
-        this.maxLength = workspace.getConfiguration(pluginName).get('maxLength') as Number
-        this.topK = workspace.getConfiguration(pluginName).get('topK') as Number
-        this.topP = workspace.getConfiguration(pluginName).get('topP') as Number
-        this.temperature = workspace.getConfiguration(pluginName).get('temperature') as Number
+        this.debounce = workspace.getConfiguration(pluginName).get('debounce') as number
+        this.maxLength = workspace.getConfiguration(pluginName).get('maxLength') as number
+        this.topK = workspace.getConfiguration(pluginName).get('topK') as number
+        this.topP = workspace.getConfiguration(pluginName).get('topP') as number
+        this.temperature = workspace.getConfiguration(pluginName).get('temperature') as number
+        this.contextLineCount = workspace.getConfiguration(pluginName).get('contextLineCount') as number
     }
 }
 
 function updateConfig(c: NccConfig) {
     c.server = workspace.getConfiguration(pluginName).get('server') as string
-    c.debounce = workspace.getConfiguration(pluginName).get('debounce') as Number
-    c.maxLength = workspace.getConfiguration(pluginName).get('maxLength') as Number
-    c.topK = workspace.getConfiguration(pluginName).get('topK') as Number
-    c.topP = workspace.getConfiguration(pluginName).get('topP') as Number
-    c.temperature = workspace.getConfiguration(pluginName).get('temperature') as Number
+    c.debounce = workspace.getConfiguration(pluginName).get('debounce') as number
+    c.maxLength = workspace.getConfiguration(pluginName).get('maxLength') as number
+    c.topK = workspace.getConfiguration(pluginName).get('topK') as number
+    c.topP = workspace.getConfiguration(pluginName).get('topP') as number
+    c.temperature = workspace.getConfiguration(pluginName).get('temperature') as number
+    c.contextLineCount = workspace.getConfiguration(pluginName).get('contextLineCount') as number
 }
 
 var config: NccConfig
