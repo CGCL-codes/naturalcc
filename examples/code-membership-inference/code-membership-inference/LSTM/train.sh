7a3e2d5463f74b4c74e1992ec5d784d8dc63f66d
guanghua233 <56295305+guanghua233@users.noreply.github.com>
2024-04-22 15:42:30 +0800

    Delete examples/code-membership-inference/code-membership-inference directory
    update

--- examples/code-membership-inference/code-membership-inference/LSTM/train.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-MODEL=shadow_9
-DATADIR=/pathto/data/membership_inference/${MODEL}/lstm
-LOGFILE=${MODEL}_model_train.log
-
-nohup python -u seqrnn.py --base_dir=${DATADIR} > logfile/${LOGFILE} 2>&1 &
