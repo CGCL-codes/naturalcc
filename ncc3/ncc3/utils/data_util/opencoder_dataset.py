f12dd55284780fb16397effa294811bf580e6bf0
Talantan1102 <545811257@qq.com>
2024-11-28 14:29:24 +0800

    refactor: tree-sitter

---
+++ ncc3/ncc3/utils/data_util/opencoder_dataset.py
@@ -0,0 +1,15 @@
+import re
+from datasets import load_dataset
+from ncc3.data_util.base_dataset import BaseDataset
+
+class OpenCoderDataset(BaseDataset):
+    def __init__(self, tokenizer, max_length=512):
+        super().__init__(tokenizer, max_length)
+
+    def load(self, type):
+        dataset = self.dataset_config["repobench_"+language]
+        dataset = load_dataset(dataset)
+
+
+
+
