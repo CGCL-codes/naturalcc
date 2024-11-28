6a6837039ebd246950de7415abe3a87787f1000b
Talantan1102 <545811257@qq.com>
2024-11-28 15:38:46 +0800

    merge examples

---
+++ examples/test_code_utilities/test_parse_code.py
@@ -0,0 +1,21 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path("").absolute().parent))
+from ncc3.utils.code_util.apex.apex_code_util import ApexCodeUtility
+
+apex_code_util = ApexCodeUtility()
+
+sample_code = """
+    public class SampleClass {    
+        public Integer myNumber;
+        public Integer getMyNumber() {
+            // Return the current value of myNumber
+            return this.myNumber;
+        }
+    }
+"""
+
+ast = apex_code_util.parse(sample_code)
+
+# This will print the tree-sitter AST object
+print(ast)
